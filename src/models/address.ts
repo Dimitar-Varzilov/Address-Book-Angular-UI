95fae35c8e840a3484b6f365fa9f6231f8a481b1
m911 <mitaka911@gmail.com>
2023-02-03 03:26:50 +0200

    1

--- src/models/address.ts
+++ src/models/address.ts
@@ -3,5 +3,5 @@ export class Address {
   firstName = '';
   lastName = '';
   telephone: string = '';
-  fullName: string = this.firstName + ' ' + this.lastName;
+  // fullName: string = this.firstName + ' ' + this.lastName;
 }
